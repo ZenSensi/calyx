ab31a6c6f31680c73341165dbe46bb6913ec1dda
Arnabh <royalfantasyyt@gmail.com>
2026-05-20 13:50:54 +0530

    chore: remove unwanted files and add .gitignore

--- test.js
+++
@@ -1,8 +0,0 @@
-fetch('http://localhost:3000/api/getToken', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ roomName: 'test', participantName: 'test' })
-})
-  .then(r => r.json())
-  .then(console.log)
-  .catch(console.error);
